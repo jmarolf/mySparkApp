2863e40270b5bc4543a1230492b7088f8a30d6a4
Jonathon Marolf <jmarolf@microsoft.com>
2019-10-13 18:06:57 -0700

    get things working

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ using Apache.Arrow.Types;
 using Microsoft.Spark.Sql;
 using Microsoft.Spark.Sql.Types;
 using StructType = Microsoft.Spark.Sql.Types.StructType;
+
+using static Microsoft.Spark.Sql.Functions;
+using System.Collections.Generic;
+
 namespace mySparkApp
 {
     class Program
@@ -19,22 +23,13 @@ namespace mySparkApp
 
             // Create initial DataFrame
             var dataFrame = spark.Read()
-                .Schema("`?xml` STRUCT<`@version`: STRING, `@encoding`: STRING>, `posts` STRUCT<`row`: ARRAY<STRUCT<`@Id`: INT, `@PostTypeId`: INT, `@CreationDate`: STRING, `@Score`: INT, `@ViewCount`: INT, `@Body`: STRING, `@OwnerUserId`: INT, `@LastActivityDate`: STRING, `@Title`: STRING, `@Tags`: STRING, `@AnswerCount`: INT, `@CommentCount`: INT, `@FavoriteCount`: INT>>>")
                 .Json(@"C:\data\3dprinting.meta.stackexchange.com\Posts.json");
 
-            var schema = dataFrame.Schema();
-
-            var words = dataFrame
-                 .Select(Functions.ArrayJoin(Functions.Col("posts.row.@Body"), " ", " ").Alias("body"))
-                 .Select(Functions.Split(Functions.Col("body"), " ").Alias("words"))
-                 .Select(Functions.Explode(Functions.Col("words"))
-                 .Alias("word"))
-                 .GroupBy("word")
-                 .Count()
-                 .OrderBy(Functions.Col("count").Desc());
-
-            // Show results
-            words.Show();
+            dataFrame
+                .Select(Split(Col("Body"), " ").As("words"))
+                .Select(Explode(Col("words")).As("word"))
+                .GroupBy("word").Count().OrderBy(Col("count").Desc())
+                .Show();
         }
     }
 }
